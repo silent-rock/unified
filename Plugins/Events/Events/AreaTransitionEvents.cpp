38eb87528d8920e8eeb7b2e860c2a4247a4f94ff
Silent <nieswiety@gmail.com>
2025-04-14 15:49:38 +0200

    Update AreaTransitionEvents.cpp

--- Plugins/Events/Events/AreaTransitionEvents.cpp
+++ Plugins/Events/Events/AreaTransitionEvents.cpp
@@ -2,90 +2,95 @@
 #include "API/CAppManager.hpp"
 #include "API/CServerExoApp.hpp"
 #include "API/CNWSArea.hpp"
-#include "API/CNWSObject.hpp"
 #include "API/CNWSCreature.hpp"
 #include "API/CNWSPlayer.hpp"
 #include "API/Functions.hpp"
 #include "API/Constants.hpp"
 
 namespace Events {
-
 using namespace NWNXLib;
 using namespace NWNXLib::API;
 using namespace NWNXLib::Services;
 
-static Hooks::Hook s_SetAreaHook;
+static Hooks::Hook s_AddToAreaHook;
+static std::unordered_map<OBJECT_ID, OBJECT_ID> s_PreviousAreaMap;
 
-static void SetAreaHook(CNWSObject*, OBJECT_ID);
+static void AddToAreaHook(CNWSCreature*, CNWSArea*, float, float, float, BOOL);
 
 void AreaTransitionEvents() __attribute__((constructor));
 void AreaTransitionEvents()
 {
     InitOnFirstSubscribe("NWNX_ON_AREA_TRANSITION_.*", []() {
-        s_SetAreaHook = Hooks::HookFunction(&CNWSObject::SetArea,
-            &SetAreaHook, Hooks::Order::Early);
+        s_AddToAreaHook = Hooks::HookFunction(&CNWSCreature::AddToArea,
+            &AddToAreaHook, Hooks::Order::Early);
     });
 }
 
-static void SetAreaHook(CNWSObject* pObject, OBJECT_ID oidArea)
+static void AddToAreaHook(CNWSCreature* pCreature, CNWSArea* pArea, float x, float y, float z, BOOL bRunScripts)
 {
-    // Check if the object is a creature
-    if (pObject->m_nObjectType != Constants::ObjectType::Creature)
+    if (!pCreature || !pArea)
     {
-        s_SetAreaHook->CallOriginal<void>(pObject, oidArea);
+        s_AddToAreaHook->CallOriginal<void>(pCreature, pArea, x, y, z, bRunScripts);
         return;
     }
 
-    // Get creature pointer
-    CNWSCreature* pCreature = Utils::AsNWSCreature(pObject);
-    
-    // Check if it's a player character
-    if (pCreature && pCreature->m_bPlayerCharacter)
+    // Only process player characters
+    if (pCreature->m_bPlayerCharacter)
     {
-        // Get area information
+        OBJECT_ID oidPlayer = pCreature->m_idSelf;
+        OBJECT_ID oidNewArea = pArea->m_idSelf;
         OBJECT_ID oidPreviousArea = pCreature->m_oidArea;
-        OBJECT_ID oidPlayer = pCreature->m_idSelf; // Store the player ID
         
-        // Check if we're actually changing areas
-        if (oidPreviousArea != oidArea)
+        // Store the previous area for after event
+        s_PreviousAreaMap[oidPlayer] = oidPreviousArea;
+        
+        // Only fire event if actually changing areas
+        if (oidPreviousArea != oidNewArea)
         {
-            // Prepare data for BEFORE event
+            // Push event data for BEFORE event
             PushEventData("PREVIOUS_AREA", Utils::ObjectIDToString(oidPreviousArea));
-            PushEventData("TARGET_AREA", Utils::ObjectIDToString(oidArea));
+            PushEventData("TARGET_AREA", Utils::ObjectIDToString(oidNewArea));
             
             // Signal BEFORE event
             std::string result;
             if (!SignalEvent("NWNX_ON_AREA_TRANSITION_BEFORE", oidPlayer, &result))
             {
-                // Event was skipped, so don't perform the area change
+                // Event was skipped - don't change area
                 return;
             }
+        }
+    }
+    
+    // Call original function
+    s_AddToAreaHook->CallOriginal<void>(pCreature, pArea, x, y, z, bRunScripts);
+    
+    // Process AFTER event
+    if (pCreature->m_bPlayerCharacter)
+    {
+        OBJECT_ID oidPlayer = pCreature->m_idSelf;
+        
+        // Get previous area from map
+        auto it = s_PreviousAreaMap.find(oidPlayer);
+        if (it != s_PreviousAreaMap.end())
+        {
+            OBJECT_ID oidPreviousArea = it->second;
+            OBJECT_ID oidCurrentArea = pCreature->m_oidArea;
             
-            // Call the original function
-            s_SetAreaHook->CallOriginal<void>(pObject, oidArea);
-            
-            // After calling the original function, we need to get the creature pointer again
-            // as it might have changed or been invalidated
-            CNWSCreature* pCreatureAfter = Utils::AsNWSCreature(Utils::GetGameObject(oidPlayer));
-            
-            // Make sure the creature still exists and the area change happened
-            if (pCreatureAfter && pCreatureAfter->m_oidArea != oidPreviousArea)
+            // Only fire event if actually changed areas
+            if (oidPreviousArea != oidCurrentArea)
             {
-                // Prepare data for AFTER event
+                // Push event data for AFTER event
                 PushEventData("PREVIOUS_AREA", Utils::ObjectIDToString(oidPreviousArea));
-                PushEventData("TARGET_AREA", Utils::ObjectIDToString(pCreatureAfter->m_oidArea));
+                PushEventData("TARGET_AREA", Utils::ObjectIDToString(oidCurrentArea));
                 
                 // Signal AFTER event
                 SignalEvent("NWNX_ON_AREA_TRANSITION_AFTER", oidPlayer);
             }
             
-            // We handled the whole flow, return now
-            return;
+            // Remove from map to avoid memory leaks
+            s_PreviousAreaMap.erase(it);
         }
     }
-    
-    // If we get here, just call the original without any event
-    s_SetAreaHook->CallOriginal<void>(pObject, oidArea);
 }
 
 }
